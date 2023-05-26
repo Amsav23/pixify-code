818dcae4528b0b033ef2e97614b3f8cb30197fd6
Amsav23 <130859471+Amsav23@users.noreply.github.com>
2023-05-26 18:35:06 -0400

    dark mode light mode works

--- scripts/script2.js
+++ scripts/script2.js
@@ -2,12 +2,12 @@ const themeButton = document.querySelector(".theme-btn");
 const toggleTheme = () => {
   const body = document.querySelector("body");
 
-  if(body.style.backgroundColor === "#36454f") {
+  if(body.style.backgroundColor === "rgb(42, 59, 73)") {
     body.style.backgroundColor = "white";
     themeButton.innerHTML = "Dark Mode";
 
   } else {
-    body.style.backgroundColor = "#36454f";
+    body.style.backgroundColor = "rgb(42, 59, 73)";
     themeButton.innerHTML = "Light Mode";
   }
 }
