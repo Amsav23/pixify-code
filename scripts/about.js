cf1b92170a213b0f639cecb788178b09f7720a6c
Amsav23 <130859471+Amsav23@users.noreply.github.com>
2023-05-28 12:57:28 -0400

    js

--- scripts/about.js
+++ scripts/about.js
@@ -3,17 +3,20 @@
  toggleTheme = () => {
  body = document.querySelector("body");
  const mainAbout = document.querySelector(".main-about");
+ const submitBorder = document.querySelector("submit-btn");
 
 
   if(body.style.backgroundColor === "rgb(42, 59, 73)") {
     body.style.backgroundColor = "white";
     mainAbout.style.color = "black";
+    //submitBorder.style.border = "initial";
     themeButton.innerHTML = "Dark Mode";
 
 
   } else {
     body.style.backgroundColor = "rgb(42, 59, 73)";
     mainAbout.style.color = "white";
+    submitBorder.style.border = "2px solid rgb(127,255,212)";
     themeButton.innerHTML = "Light Mode";
   }
 }
